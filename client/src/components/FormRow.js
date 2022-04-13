a9e1f5cf4db4511bc2411d315b3a256ce6afb190
ali-sorkhi <ali.sorkhi92@gmail.com>
2022-04-13 13:15:14 +0430

    register alert

--- client/src/components/FormRow.js
+++ client/src/components/FormRow.js
@@ -1,18 +1,18 @@
 const FormRow = ({ type, name, value, handleChange, labelText }) => {
   return (
-    <div className='form-row'>
-      <label htmlFor={name} className='form-label'>
-        {labelText || name}
+    <div className="form-row">
+      <label htmlFor={labelText} className="form-label">
+        {labelText}
       </label>
       <input
         type={type}
         value={value}
         name={name}
         onChange={handleChange}
-        className='form-input'
+        className="form-input"
       />
     </div>
-  )
-}
+  );
+};
 
-export default FormRow
+export default FormRow;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,29 +1,35 @@
 import { useState, useEffect } from "react";
 import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
 
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
-  showAlert: false,
 };
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
+
+  const { isLoading, showAlert, displayAlert } = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
 
   const handleChange = (e) => {
-    console.log(e.target);
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
   };
 
   return (
@@ -31,24 +37,27 @@ const Register = () => {
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>{values.isMember ? "ورود" : "ثبت نام"}</h3>
-        {values.showAlert && <Alert />}
+        {showAlert && <Alert />}
         {!values.isMember && (
           <FormRow
             type="text"
-            name="نام"
+            labelText="نام"
+            name="name"
             value={values.name}
             handleChange={handleChange}
           />
         )}
         <FormRow
           type="email"
-          name="ایمیل"
+          labelText="ایمیل"
+          name="email"
           value={values.email}
           handleChange={handleChange}
         />
         <FormRow
           type="password"
-          name="کلمه عبور"
+          labelText="کلمه عبور"
+          name="password"
           value={values.password}
           handleChange={handleChange}
         />
