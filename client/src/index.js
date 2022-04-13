8d5ec093c31671f266881f2d925b9b862fb73547
ali-sorkhi <ali.sorkhi92@gmail.com>
2022-04-13 09:14:38 +0430

    persian fonts

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css";
+import "normalize.css"; //npm install normalize.css and should be imported before index.css
 import "./index.css";
 import App from "./App";
 
