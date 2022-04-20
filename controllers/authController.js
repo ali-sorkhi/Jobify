4a787e0fb5f06f664c3a79f5eabe74fa43be3478
ali-sorkhi <ali.sorkhi92@gmail.com>
2022-04-20 13:06:23 +0430

    login back-end

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError } from "../errors/index.js";
+import { BadRequestError, UnauthenticatedError } from "../errors/index.js";
 
 const register = async (req, res, next) => {
   /* try {
@@ -11,20 +11,28 @@ const register = async (req, res, next) => {
   } */
   // *** if we use express-async-errors package we can remove try{}catch{}
   // this means error will automatically be sent to error handler
+
+  //getting data from front-end
   const { name, email, password } = req.body;
+
+  //check if all fields are filled
   if (!name || !email || !password)
     throw new BadRequestError("لطفاً همه فیلدها را پر نمایید");
 
+  //find if email already exist
   const userAlreadyExists = await User.findOne({ email });
   if (userAlreadyExists) {
     throw new BadRequestError("ایمیل تکراری می باشد");
   }
+
   //creating user in DB:
   const user = await User.create({ name, email, password });
+
   //creating JWT:
   const token = user.createJWT();
 
-  res.status(StatusCodes.OK).json({
+  //sending response to font-end
+  res.status(StatusCodes.CREATED).json({
     user: {
       email: user.email,
       name: user.name,
@@ -37,7 +45,41 @@ const register = async (req, res, next) => {
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  //get email and password from front-end
+  const { email, password } = req.body;
+
+  //check if all fields are filled
+  if (!email || !password)
+    throw new BadRequestError("لطفاً همه فیلدها را پر نمایید");
+
+  //find the user and add password
+  const user = await User.findOne({ email }).select("+password");
+
+  //throw error if user not found
+  if (!user) {
+    throw new UnauthenticatedError("نام کاربری یا کلمه عبور اشتباه می باشد.");
+  }
+
+  //compare user password
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  //throw error if password is incorrect
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("نام کاربری یا کلمه عبور اشتباه می باشد.");
+  }
+
+  //creating JWT:
+  const token = user.createJWT();
+
+  //remove the password from response
+  user.password = undefined;
+
+  //sending response to font-end
+  res.status(StatusCodes.OK).json({
+    user,
+    token,
+    location: user.location,
+  });
 };
 
 const updateUser = async (req, res) => {
--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,6 @@
 //for adding error statusCode to Error Class
 import BadRequestError from "./bad-request.js";
 import NotFoundError from "./not-found.js";
+import UnauthenticatedError from "./unauthenticated.js";
 
-export { BadRequestError, NotFoundError };
+export { BadRequestError, NotFoundError, UnauthenticatedError };
